ae6a4b19e8dc90a8ae8baa3346c2f13a0e2cf781
ryanalco <RyanAlco@ryans-macbook-pro.local>
2017-10-03 12:14:13 -0700

    Initial Commit

---
+++ HW1-10C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1-10C
+//
+//  Created by Ryan Alvarez-Cohen on 10/3/17.
+//  Copyright © 2017 Ryan Alvarez-Cohen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
